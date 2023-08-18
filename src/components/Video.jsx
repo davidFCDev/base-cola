a0fb706cf92a5ea3314e942fe4939719c2c88146
David Fernández <davidfernandezcomesana@gmail.com>
2023-08-18 12:25:40 +0200

    arreglos

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -13,11 +13,11 @@ export const Video = () => {
   const [ref3, inView3] = useInView({ threshold: 0.6, triggerOnce: true });
 
   return (
-    <div className="w-full flex flex-col gap-10 py-20 justify-center items-center text-center">
+    <div className="w-full flex flex-col gap-10 py-20 px-4 justify-center items-center text-center">
       <h1 className="text-primary text-4xl sm:text-5xl title">
         Coinbase Onchain <span className="text-red-600">Summer</span>
       </h1>
-      <h3 className="tracking-widest flex flex-col gap-2 text-xl sm:text-2xl w-[75%] md:w-[58%]">
+      <h3 className="tracking-widest flex flex-col gap-2 text-xl sm:text-2xl w-[85%] md:w-[58%]">
         A multi-week celebration of onchain art, music, and more, powered by
         Base. Cocacola is bringing their Global Masterpiece campaign onchain
         with iconic works from leading artists, and we helped them with the
